ba718e4ec5c12accdfec91fcc2793dd0e9149893
dcerjan <domagoj.cerjan@gmail.com>
2019-07-21 03:23:58 +0200

    Add postcss nested, add react transition group, add a portal target in index.html

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -93,6 +93,7 @@ module.exports = function(webpackEnv) {
           plugins: () => [
             require('postcss-import')(),
             require('postcss-flexbugs-fixes'),
+            require('postcss-nested'),
             require('postcss-preset-env')({
               autoprefixer: {
                 flexbox: 'no-2009',
